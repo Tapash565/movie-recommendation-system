61186add2a4b87e2c5db2b812d18961a6e89070f
Tapash Kumar <kumar.tapash565@gmail.com>
2026-02-10 16:14:31 +0530

    CORS issue fix#1

--- backend/app/main.py
+++ backend/app/main.py
@@ -65,7 +65,7 @@ app.add_middleware(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://movie-recommendation-system-phi-lac.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
